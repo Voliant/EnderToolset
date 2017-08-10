4166ce4608e382ed1f5a75d88772e68d315ab6bd
Voliant <thatvaliantvole@gmail.com>
2017-08-10 15:42:16 +0100

    Added ender shovel & hoe along with rendering for all tools

--- src/main/java/valiantvole/endertoolset/items/ModItems.java
+++ src/main/java/valiantvole/endertoolset/items/ModItems.java
@@ -19,18 +19,26 @@ public class ModItems {
 	public static ItemSword enderSword = new ItemSword(EnderToolset.enderToolMaterial, "ender_sword");
 	public static ItemAxe enderAxe = new ItemAxe(EnderToolset.enderToolMaterial, "ender_axe");
 	public static ItemPickaxe enderPickaxe = new ItemPickaxe(EnderToolset.enderToolMaterial, "ender_pickaxe");
+	public static ItemShovel enderShovel = new ItemShovel(EnderToolset.enderToolMaterial, "ender_shovel");
+	public static ItemHoe enderHoe = new ItemHoe(EnderToolset.enderToolMaterial, "ender_hoe");
 
 	public static void register(IForgeRegistry<Item> registry) {
 		registry.registerAll(    
 				enderSword,
 				enderAxe,
-				enderPickaxe
+				enderPickaxe,
+				enderShovel,
+				enderHoe
 				
 	    );      
 	}
 
 	public static void registerModels() {
 		enderSword.registerItemModel(enderSword);
+		enderAxe.registerItemModel(enderAxe);
+		enderPickaxe.registerItemModel(enderPickaxe);
+		enderShovel.registerItemModel(enderShovel);
+		enderHoe.registerItemModel(enderHoe);
 	}
 
 	public static void init() {
